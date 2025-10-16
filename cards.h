1c5002b78ad4109151acf8dbf25c7fe3c2791987
TitasBSP <titasbsp@gmail.com>
2025-10-16 09:35:34 +0200

    added landscape cards

--- cards.h
+++ cards.h
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 class landscapeCard {
     public:
@@ -13,5 +14,28 @@ class mobCard {
 };
 
 landscapeCard vilLibrarian;
+landscapeCard vilCleric;
+landscapeCard vilFletcher;
+landscapeCard vilArmorsmith;
+landscapeCard vilToolsmith;
+landscapeCard vilWeaponsmith;
+landscapeCard vilCartographer;
+landscapeCard vilFarmer;
+landscapeCard woods;
+landscapeCard cherryBlossom;
+landscapeCard lushCave;
+landscapeCard geode;
+landscapeCard taiga;
+landscapeCard shipwreck;
+landscapeCard jungle;
+landscapeCard desertTemple;
+landscapeCard illagerOutpost;
+landscapeCard mineshaft;
+landscapeCard witchHut;
+landscapeCard chasm;
+landscapeCard ruinedPortal;
+landscapeCard deepCave;
+landscapeCard woodlandMansion;
+landscapeCard oceanMonument;
 
 
